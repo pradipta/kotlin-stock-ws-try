4055dc19321bb14a1753a8a461fbd2d2a8ace74c
Pradipta Sarma <pradiptasarma@outlook.com>
2020-09-27 19:37:06 +0530

    Initial

---
+++ src/test/kotlin/com/pradipta/kotlinstockwstry/KotlinStockWsTryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pradipta.kotlinstockwstry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStockWsTryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
